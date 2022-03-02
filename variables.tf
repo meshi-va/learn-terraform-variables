c00182b1401d5165f04c8732b2e1222a3fe75e8f
meshi-va <ev.ellerine@gmail.com>
2022-03-02 09:57:47 +0200

    Added vpn gateway variable

--- variables.tf
+++ variables.tf
@@ -10,3 +10,15 @@ variable "vpc_cidr_block" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "instance_count" {
+  description = "Number of instances to provision."
+  type        = number
+  default     = 2
+}
+
+variable "enable_vpn_gateway" {
+  description = "Enable a VPN gateway in your VPC."
+  type        = bool
+  default     = false
+}
